a3e06111721017c717d05e83e25889e035f24151
datagutten <datagutten@datagutten.net>
2017-12-02 22:41:09 +0100

    Improve search class

--- tidalsearch.class.php
+++ tidalsearch.class.php
@@ -1,32 +1,43 @@
 <?php
-require 'tidalinfo.class.php';
+require_once 'TIDALtools/tidalinfo.class.php';
 class tidalsearch extends tidalinfo
 {
-	function track($artist,$title)
+	function __construct()
 	{
-		echo "Tittel: ".$title."\n";
-		$search=preg_replace('/(.+) \(?feat.+/','$1',$title,-1,$count); //Search with plain title
-		echo "Search: ".$search."\n";
+		parent::init_curl();
+	}
+	//Remove featured artists from title
+	function clean_title($title)
+	{
+		return preg_replace('/(.+) \(?feat.+/','$1',$title,-1,$count); //Search with plain title
+	}
+	function clean_artist($artist)
+	{
 		$artist_plain=preg_replace('/(.+) feat.+/','$1',$artist,-1,$count);
 		//var_dump($artist_plain);
 		$artist_plain=preg_replace('/(.+?) &.+/','$1',$artist_plain,-1,$count);
-		//var_dump($artist_plain);
-		$matches=$this->api_request('search','tracks','','&limit=20&query='.urlencode($search));
-		if($matches===false)
-			return false;
-		echo sprintf("Artist_plain: %s\n",$artist_plain);
-		$artist_plain=str_replace(', ',"\n",$artist_plain); //Separate artists by line break instead of comma
+		return $artist_plain;
+	}
+	function search_track($search)
+	{
+		return $this->api_request('search','tracks','','&limit=20&query='.urlencode($search));
+	}
+	function search_album($search)
+	{
+		return $this->api_request('search','albums','','&limit=20&query='.urlencode($search));
+	}
+	
+	function verify_artist($search_result,$wanted_artist)
+	{
+		$wanted_artist=str_replace(', ',"\n",$wanted_artist); //Separate artists by line break instead of comma
 		foreach($matches['items'] as $item)
 		{
 			//print_r($item);
 			$artists=implode("\n",array_column($item['artists'],'name')); //Create a string from the search result artist array
-			if(mb_stripos($artists,$artist_plain)!==false)
+			if(mb_stripos($artists,$wanted_artist)!==false) //Current search result has the wanted artist
 			{
-				$match=$item;
-				break;
+				return $item;
 			}
-			else
-				unset($match);
 		}
 	}
 }
