56612b15fdc1dd913f13e7fb2291970f3fd452db
datagutten <datagutten@datagutten.net>
2019-07-13 19:29:33 +0200

    Add tests for get_id

---
+++ tests/GetIdTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace datagutten\tidal\tests;
+
+use datagutten\Tidal\Info;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class GetIdTest extends TestCase
+{
+    function testId()
+    {
+        $id = Info::get_id('19226924');
+        $this->assertEquals(19226924, $id);
+    }
+    function testAlbumURL()
+    {
+        $id = Info::get_id('https://tidal.com/browse/album/19226924', 'album');
+        $this->assertEquals(19226924, $id);
+        $id = Info::get_id('https://tidal.com/browse/album/19226924');
+        $this->assertEquals(19226924, $id);
+    }
+    function testArtistURL()
+    {
+        $id = Info::get_id('https://tidal.com/browse/artist/3963574', 'artist');
+        $this->assertEquals(3963574, $id);
+        $id = Info::get_id('https://tidal.com/browse/artist/3963574');
+        $this->assertEquals(3963574, $id);
+    }
+    function testTrackURL()
+    {
+        $id = Info::get_id('https://tidal.com/browse/track/19226925', 'track');
+        $this->assertEquals(19226925, $id);
+        $id = Info::get_id('https://tidal.com/browse/track/19226925');
+        $this->assertEquals(19226925, $id);
+    }
+    function testMismatchURL()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid album URL: https://tidal.com/browse/track/19226925');
+        Info::get_id('https://tidal.com/browse/track/19226925', 'album');
+    }
+    function testInvalidURL()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unable to find id from URL: https://tidal.com');
+        Info::get_id('https://tidal.com');
+    }
+}
