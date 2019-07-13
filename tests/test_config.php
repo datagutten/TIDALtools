5f053baf0dbc87ac88e510794e8615ea9db3611f
datagutten <datagutten@datagutten.net>
2019-07-13 21:23:47 +0200

    Test with file paths inside test directory

--- tests/test_config.php
+++ tests/test_config.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['input_path_id']='/home/music/id_input';
-$config['input_path_order']='/home/music/order_input';
-$config['output_path']='/home/music';
+$config['input_path_id']=__DIR__.'/sample_data/id_input';
+$config['input_path_order']=__DIR__.'/sample_data/order_input';
+$config['output_path']=__DIR__.'/rename_output';
 
 return $config;
