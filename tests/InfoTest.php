916a6d7f97f027b1536e520d43b5188173a2e0a9
datagutten <datagutten@datagutten.net>
2019-07-13 16:23:31 +0200

    Add more tests

--- tests/InfoTest.php
+++ tests/InfoTest.php
@@ -46,4 +46,66 @@ class InfoTest extends TestCase
         $this->expectException(Tidal\TidalError::class);
         $this->tidal->query('https://httpbin.org/status/codes/500');
     }
+
+    /**
+     * @throws Tidal\TidalError
+     */
+    public function testResponseNotString()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        /** @noinspection PhpParamsInspection */
+        $this->tidal->parse_response(['foo']);
+    }
+
+    /**
+     * @throws Tidal\TidalError
+     */
+    public function testExceptionMessage()
+    {
+        $this->expectException(Tidal\TidalError::class);
+        $this->expectExceptionMessage('test_message');
+        $this->tidal->parse_response('{"userMessage": "test_message"}');
+    }
+
+    /**
+     * @throws Tidal\TidalError
+     */
+    public function testQueryWithoutToken()
+    {
+        $this->expectException(Tidal\TidalError::class);
+        $this->expectExceptionMessage('Missing token');
+        $this->tidal->artist('https://tidal.com/browse/artist/5496411');
+    }
+
+    public function testArtistImage()
+    {
+        $image = Tidal\Info::resolve_image(['picture'=>'37b52f9d-eb8e-41e4-8a53-e6a5af704ec3']);
+        $this->assertArrayHasKey('picture', $image);
+        $this->assertEquals('https://resources.wimpmusic.com/images/37b52f9d/eb8e/41e4/8a53/e6a5af704ec3/320x320.jpg', $image['picture']);
+        $response = Requests::head($image['picture']);
+        $this->assertEquals(200, $response->status_code);
+    }
+
+    /**
+     * @throws Tidal\TidalError
+     */
+    public function testAlbumCover()
+    {
+        $album = $this->tidal->album('https://tidal.com/browse/album/80219164');
+        $this->assertIsArray($album);
+        $this->assertEquals('Hva nå', $album['title']);
+        $response = Requests::head($album['cover']);
+        $this->assertEquals(200, $response->status_code);
+    }
+
+    /**
+     * @throws Tidal\TidalError
+     */
+    public function testArtist()
+    {
+        $this->tidal->token = Tidal\Info::get_token();
+        $artist = $this->tidal->artist('https://tidal.com/browse/artist/5496411');
+        $this->assertIsArray($artist);
+        $this->assertEquals('No. 4', $artist['title']);
+    }
 }
