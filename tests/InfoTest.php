bc1b519be990aedd3c49a8957510f3ca45b91bcb
datagutten <datagutten@datagutten.net>
2020-03-28 10:22:14 +0100

    Update test after data change

--- tests/InfoTest.php
+++ tests/InfoTest.php
@@ -199,7 +199,7 @@ class InfoTest extends TestCase
         $albums = $this->tidal->artist_albums('https://tidal.com/browse/artist/5496411');
         $this->assertIsArray($albums);
         $this->assertArrayHasKey('items', $albums);
-        $this->assertEquals('Hva nå', $albums['items'][0]['title']);
+        $this->assertEquals('Duell', $albums['items'][0]['title']);
     }
 
     /**
